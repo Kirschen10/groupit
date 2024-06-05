ca7f15d3c891f402bd16a18dd3c6cbf11706ebf8
mayshalon <mayshalom@campus.technion.ac.il>
2024-06-05 16:48:05 +0300

    connect to Azure

--- src/LogIn.js
+++ src/LogIn.js
@@ -19,7 +19,7 @@ const LogIn = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch('/login', {
+            const response = await fetch('http://localhost:8081/users_data', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -32,11 +32,12 @@ const LogIn = () => {
             if (response.ok) {
                 navigate('/HomePage');
             } else {
-                setError(data.message);
+                setError(data.message || 'An error occurred. Please try again later.');
             }
         } catch (err) {
-        setError('An error occurred. Please try again later. Details:' + err.message);
-    }
+            setError('An error occurred. Please try again later. Details: ' + err.message);
+            console.error('Fetch error:', err);
+        }
     };
 
     const handleRegistration = (e) => {
@@ -65,8 +66,8 @@ const LogIn = () => {
                         />
                     </div>
                     <div className="form-group-LogIn">
-                    <div className="icon-container-LogIn">
-                            <img src="\Images\padlock.png" height={"20px"} alt="Username Icon" className="icon" />
+                        <div className="icon-container-LogIn">
+                            <img src="\Images\padlock.png" height={"20px"} alt="Password Icon" className="icon" />
                         </div>
                         <input
                             type="password"
