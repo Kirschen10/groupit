20fc7bca67f188c87a6ed4718228a834c9c3c3de
Daniellecoh3107 <Daniellecoh3107@gmail.com>
2024-06-29 18:05:08 +0300

    danielle and ofek 29.06 18:05

--- src/JoinGroup.js
+++ src/JoinGroup.js
@@ -176,100 +176,100 @@ const JoinGroup = () => {
 
 
     return (
-    <div className={"background-join"}>
-        <div>
-            <span className="Home-page-button-join" onClick={handleHomePage}>
-                <img src="/Images/Logo.svg" alt="Logo" />
-            </span>
-        </div>
-        <div>
-            <span className="profile-button-join" onClick={handleProfile}>
-                <img src="/Images/user.svg" alt="Profile" />
-            </span>
-        </div>
-        <div>
-            <span className="question-mark-button-join" onClick={handleQuestions}>
-                <img src="/Images/question.svg" alt="Question" />
-            </span>
-        </div>
-        <div className="join-group-container">
-            <h2>Join Group</h2>
-            <div className="join-group-top">
-                <h3 style={{ textAlign: 'left' }}>Join Group by Group ID:</h3>
-                <input
-                    type="text"
-                    value={groupID}
-                    onChange={(e) => setGroupID(e.target.value)}
-                />
-                <button onClick={handleJoinGroupByID}>Join Group</button>
+        <div className="background-joinGroup">
+            <div>
+                <span className="homeButton-joinGroup" onClick={handleHomePage}>
+                    <img src="/Images/Logo.svg" alt="Logo" />
+                </span>
             </div>
-            <div className="explore-groups-bottom">
-                <h3>Explore Groups with Similar Taste</h3>
-                <button onClick={handleToggleGroups}>
-                    {showGroups ? 'Close' : 'Find Groups'}
-                </button>
-                {showGroups && foundGroups.length > 0 && (
-                    <ul>
-                        {foundGroups.map((group) => (
-                            <li key={group.groupID} className="group-card-joingroup">
-                                <div className="group-header-joingroup">
-                                    <span className="group-name-joingroup">{group.groupName}</span>
-                                    <span className="group-id-joingroup">(ID: {group.groupID})</span>
-                                </div>
-                                <div className="group-buttons-joingroup">
-                                    <span
-                                        className="info-button-joingroup"
-                                        onMouseEnter={() => handleMouseEnter(group)}
-                                        onMouseLeave={handleMouseLeave}
-                                    >
-                                        Info
-                                    </span>
-                                    <button
-                                        className="join-button-joingroup"
-                                        onClick={() => handleJoinFoundGroup(group.groupID)}
-                                    >
-                                        Join Group
-                                    </button>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-                {showErrorMessage && feedbackMessage && <p className="feedback-message-JoinGroup">{feedbackMessage}</p>}
+            <div>
+                <span className="profileButton-joinGroup" onClick={handleProfile}>
+                    <img src="/Images/user.svg" alt="Profile" />
+                </span>
             </div>
-        </div>
-        {hoveredGroup && (
-            <div className="group-details-joingroup visible">
-                <h3>{hoveredGroup.groupName}</h3>
-                <p>{hoveredGroup.groupDescription}</p>
-                <div className="detail-item-joingroup">
-                    <span>ID:</span>
-                    <span className="value">{hoveredGroup.groupID}</span>
+            <div>
+                <span className="questionButton-joinGroup" onClick={handleQuestions}>
+                    <img src="/Images/question.svg" alt="Question" />
+                </span>
+            </div>
+            <div className="container-joinGroup">
+                <h2>Join Group</h2>
+                <div>
+                    <h3>Join Group by Group ID:</h3>
+                    <input
+                        type="text"
+                        value={groupID}
+                        onChange={(e) => setGroupID(e.target.value)}
+                    />
+                    <button onClick={handleJoinGroupByID}>Join Group</button>
                 </div>
-                <div className="detail-item-joingroup">
-                    <span>Created At:</span>
-                    <span className="value">{new Date(hoveredGroup.createdAt).toLocaleDateString()}</span>
-                </div>
-                <div className="detail-item-joingroup">
-                    <span>Group Members:</span>
-                    <span className="value">{hoveredGroup.userCount}</span>
+                <div>
+                    <h3>Explore Groups with Similar Taste</h3>
+                    <button onClick={handleToggleGroups}>
+                        {showGroups ? 'Close' : 'Find Groups'}
+                    </button>
+                    {showGroups && foundGroups.length > 0 && (
+                        <ul className="groupList-joinGroup">
+                            {foundGroups.map((group) => (
+                                <li key={group.groupID} className="groupListItem-joinGroup">
+                                    <div className="groupHeader-joinGroup">
+                                        <span className="groupName-joinGroup">{group.groupName}</span>
+                                        <span className="groupID-joinGroup">(ID: {group.groupID})</span>
+                                    </div>
+                                    <div className="groupButtons-joinGroup">
+                                        <span
+                                            className="infoButton-joinGroup"
+                                            onMouseEnter={() => handleMouseEnter(group)}
+                                            onMouseLeave={handleMouseLeave}
+                                        >
+                                            Info
+                                        </span>
+                                        <button
+                                            className="joinButton-joinGroup"
+                                            onClick={() => handleJoinFoundGroup(group.groupID)}
+                                        >
+                                            Join Group
+                                        </button>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                    {showErrorMessage && feedbackMessage && <p className="feedbackMessage-joinGroup">{feedbackMessage}</p>}
                 </div>
             </div>
-        )}
-        {showSuccessModal && (
-            <div className="modal-overlay-joingroup">
-                <div className="modal-content-joingroup">
-                    <h2>Successfully Joined the Group</h2>
-                    <p>Would you like to go to the group page or stay on this page?</p>
-                    <div className="modal-buttons-joingroup">
-                        <button className="modal-button-joingroup" onClick={handleGoToGroup}>Go to Group Page</button>
-                        <button className="modal-button-joingroup modal-cancel-button-joingroup" onClick={handleStay}>Stay on this Page</button>
+            {hoveredGroup && (
+                <div className="groupDetails-joinGroup visible">
+                    <h3>{hoveredGroup.groupName}</h3>
+                    <p>{hoveredGroup.groupDescription}</p>
+                    <div className="detailItem-joinGroup">
+                        <span>ID:</span>
+                        <span className="value">{hoveredGroup.groupID}</span>
+                    </div>
+                    <div className="detailItem-joinGroup">
+                        <span>Created At:</span>
+                        <span className="value">{new Date(hoveredGroup.createdAt).toLocaleDateString()}</span>
+                    </div>
+                    <div className="detailItem-joinGroup">
+                        <span>Group Members:</span>
+                        <span className="value">{hoveredGroup.userCount}</span>
                     </div>
                 </div>
-            </div>
-        )}
-    </div>
-);
+            )}
+            {showSuccessModal && (
+                <div className="modalOverlay-joinGroup">
+                    <div className="modalContent-joinGroup">
+                        <h2>Successfully Joined the Group</h2>
+                        <p>Would you like to go to the group page or stay on this page?</p>
+                        <div className="modalButtons-joinGroup">
+                            <button className="modalButton-joinGroup" onClick={handleGoToGroup}>Go to Group Page</button>
+                            <button className="modalButton-joinGroup modalCancelButton-joinGroup" onClick={handleStay}>Stay on this Page</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default JoinGroup;
